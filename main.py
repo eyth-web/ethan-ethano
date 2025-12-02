b86a0aa61b714b9cefa583d921d98ba409d0d684
eyth-web <eyth@udel.edu>
2025-12-02 16:35:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -20,19 +20,6 @@ def index(state: State) -> Page:
 
 assert_equal(index(State()), Page(State(), ["Hello World!"]))
 
-set_site_information(
-    author="your_email@udel.edu",
-    description="""A brief description of what your website does.
-    Use a triple quoted string if you want to span multiple lines.""",
-    sources=["List any help resources or sources you used"],
-    planning=["your_planning_document.pdf"],
-    links=["https://github.com/your-username/your-repo"]
-)
-hide_debug_information()
-set_website_title("Your Website Title")
-set_website_framed(False)
-
-
 
 
 start_server(State())
